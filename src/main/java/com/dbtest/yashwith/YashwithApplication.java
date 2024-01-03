736aedfb8cfc81c34f1e0051c7658948070348c4
Yashwith D Alva <yashwithalva@changejar.in>
2024-01-03 14:36:25 +0530

    Initial Setup

---
+++ src/main/java/com/dbtest/yashwith/YashwithApplication.java
@@ -0,0 +1,13 @@
+package com.dbtest.yashwith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YashwithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YashwithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbtest/yashwith/YashwithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbtest.yashwith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YashwithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
